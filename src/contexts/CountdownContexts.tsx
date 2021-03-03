3b4ef6deb2ded3228298dd3f3e0f4bca27be8ad5
BrunoMatosSilva <brunomatos2010@outlook.com>
2021-03-03 17:51:01 -0300

    v1.1

--- src/contexts/CountdownContexts.tsx
+++ src/contexts/CountdownContexts.tsx
@@ -35,7 +35,7 @@ export function CountdownProvider({ children }: ChallegensProviderProps) {
     function resetCountDown() {
         clearTimeout(countdownTimeout)
         setIsActive(false)
-        setTime(0.1 * 60)
+        setTime(25 * 60)
         setHasFinished(false)
     }
 
